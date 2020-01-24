9063d462adfb9b7e212691443f8cb61892e00d31
Vick Javariz <51038821+javavick@users.noreply.github.com>
2020-01-23 22:13:58 -0400

    Complete MVP

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,18 +1,45 @@
+const Data = require("../users/userDb.js");
+
 const logger = (req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} on ${req.url}`);
   next();
 };
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  Data.getById(req.params.id)
+    .then((user) => {
+      if (user.length > 0) {
+        req.user = user;
+        next();
+      } else {
+        res.status(400).json({ message: "Invalid user ID." });
+      }
+    })
+    .catch(() =>
+      res
+        .status(500)
+        .json({ error: "There was an issue getting the specified user." })
+    );
 }
 
 function validateUser(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "Missing user data." });
+  } else if (!req.body.name) {
+    res.status(400).json({ message: "Missing required name field." });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "Missing post data." });
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "Missing required text field." });
+  } else {
+    next();
+  }
 }
 
 module.exports = { logger, validateUser, validateUserId, validatePost };
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,15 +1,31 @@
 const express = require("express");
-const Data = require("./userDb.js");
+const Users = require("./userDb.js");
+const Posts = require("../posts/postDb.js");
+const {
+  validateUser,
+  validateUserId,
+  validatePost
+} = require("../middleware/middleware.js");
 const router = express.Router();
 
-router.post("/", (req, res) => {});
+router.post("/", validateUser, (req, res) => {
+  Users.insert(req.body)
+    .then((newPost) => res.status(201).json(newPost))
+    .catch(() =>
+      res.status(500).json({ error: "There was an error adding the user." })
+    );
+});
 
-router.post("/:id/posts", (req, res) => {
-  // do your magic!
+router.post("/:id/posts", validatePost, validateUserID, (req, res) => {
+  Posts.insert(req.body)
+    .then((newPost) => res.status(201).json(newPost))
+    .catch(() =>
+      res.status(500).json({ error: "There was an error adding the user." })
+    );
 });
 
 router.get("/", (req, res) => {
-  Data.get()
+  Users.get()
     .then((users) => res.status(200).json(users))
     .catch(() =>
       res.status(500).json({ error: "The data could not be acquired." })
@@ -17,7 +33,7 @@ router.get("/", (req, res) => {
 });
 
 router.get("/:id", (req, res) => {
-  Data.getById(req.params.id)
+  Users.getById(req.params.id)
     .then((user) => res.status(200).json(user))
     .catch(() =>
       res
@@ -26,16 +42,36 @@ router.get("/:id", (req, res) => {
     );
 });
 
-router.get("/:id/posts", (req, res) => {
-  // do your magic!
+router.get("/:id/posts", validateUserId, (req, res) => {
+  Users.getUserPosts(req.params.id)
+    .then((posts) => {
+      if (posts.length <= 0) {
+        res.status(404).json({ error: "The posts could not be found." });
+      } else {
+        res.status(200).json(posts);
+      }
+    })
+    .catch(() =>
+      res.status(500).json({
+        error: "The posts for the specified user could not be retrieved."
+      })
+    );
 });
 
-router.delete("/:id", (req, res) => {
-  // do your magic!
+router.delete("/:id", validateUserId, (req, res) => {
+  Users.remove(req.params.id)
+    .then((records) =>
+      res.status(200).json({ numberOfDeletedRecords: records })
+    )
+    .catch(() =>
+      res.status(500).json({ error: "The user could not be removed." })
+    );
 });
 
-router.put("/:id", (req, res) => {
-  // do your magic!
+router.put("/:id", validateUser, validateUserId, (req, res) => {
+  Users.update(req.params.id, req.body)
+    .then((count) => res.status(200).json({ numberOfUpdatedRecords: count }))
+    .catch(() => res.status(500).json({ error: "Could not update the user." }));
 });
 
 module.exports = router;
